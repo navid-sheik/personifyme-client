0de340f49c153690fc93789d93f4723976feab04
navid-sheikh <navidsheikh2020@gmail.com>
2023-07-24 14:54:14 +0100

    Initialise Xcode & Connection To RestApi

--- PersonifyMe/ViewController.swift
+++ PersonifyMe/ViewController.swift
@@ -7,11 +7,43 @@
 
 import UIKit
 
+struct ResponseData : Codable{
+    let message : String
+}
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Something ")
+        
+        let url  = URL(string: "http://localhost:4050/")!
+        let task  = URLSession.shared.dataTask(with: url){ (data, response, error) in
+            if let error =  error{
+                print("Error : \(error)")
+            }else  if let data =  data{
+               
+                let decoder  =  JSONDecoder()
+                if let responseData  =  try? decoder.decode(ResponseData.self, from: data){
+                    print("Result : \(responseData.message)")
+                }else{
+                    print("Unable to decode the json response")
+                    
+                }
+            
+            }
+            
+            
+            
+        }
+        
+        task.resume()
+        
+     
     }
 
 
